a15d49595bc38f81c9ea0ba9b7c8103c52320a64
mat <79723385+pitouemat@users.noreply.github.com>
2025-02-09 20:31:27 -0300

    Update tocarMusic.js

--- GroovIt 2.0/js/tocarMusic.js
+++ GroovIt 2.0/js/tocarMusic.js
@@ -3,70 +3,63 @@ let playBtn = document.getElementById('playBtn');
 let pauseBtn = document.getElementById('pauseBtn');
 let progressBar = document.getElementById('progressBar');
 
-// Atualiza a barra de progresso enquanto a música toca
-audioPlayer.addEventListener('timeupdate', () => {
-    progressBar.max = audioPlayer.duration;
-    progressBar.value = audioPlayer.currentTime;
-});
-
-// Permite que o usuário mude o tempo da música arrastando a barra
-progressBar.addEventListener('input', () => {
-    audioPlayer.currentTime = progressBar.value;
-});
-
-document.getElementById('pauseBtn').addEventListener('click', (e) => {
+// Botão de pausa
+pauseBtn.addEventListener('click', (e) => {
     e.preventDefault();
-
     playBtn.style.display = 'inline';
     pauseBtn.style.display = 'none';
-
-    audioPlayer.pause()
-
-
+    audioPlayer.pause();
     return false;
 });
 
-
-document.getElementById('playBtn').addEventListener('click', (e) => {
+// Botão de play
+playBtn.addEventListener('click', (e) => {
     e.preventDefault();
-
     playBtn.style.display = 'none';
     pauseBtn.style.display = 'inline';
-
-    audioPlayer.play()
-
-
+    audioPlayer.play();
     return false;
 });
 
+
 const playSong = (file) => {
-    
-
-    audioPlayer.src = file
-
+    audioPlayer.src = file;
     audioPlayer.play();
-
     playBtn.style.display = "none";
     pauseBtn.style.display = "inline";
-}
+};
 
-document.querySelectorAll('div.card_music').forEach(item =>{
-    item.addEventListener('click',event =>{
+
+document.querySelectorAll('div.card_music').forEach(item => {
+    item.addEventListener('click', event => {
         let image = item.getAttribute('data-image');
         let artist = item.getAttribute('data-artist');
         let song = item.getAttribute('data-song');
         let file = item.getAttribute('data-file');
 
-        let PlayerArtsitComponent = document.getElementById('player_artist')
-
-        PlayerArtsitComponent.innerHTML = `<img src ="`+image+`" /> <h3>`+artist+`<br><span>`+song+`</span></h3>`
+        let PlayerArtistComponent = document.getElementById('player_artist');
+        PlayerArtistComponent.innerHTML = `<img src ="${image}" /> <h3>${artist}<br><span>${song}</span></h3>`;
 
         playSong(file);
 
         let player = document.querySelector('div.player');
         player.style.visibility = 'visible';
-        
-    })
-})
+    });
+});
 
 
+audioPlayer.addEventListener('timeupdate', () => {
+    if (!isNaN(audioPlayer.duration)) {
+        progressBar.max = audioPlayer.duration;
+        progressBar.value = audioPlayer.currentTime;
+    }
+});
+
+
+progressBar.addEventListener('input', () => {
+    let wasPlaying = !audioPlayer.paused; 
+    audioPlayer.currentTime = progressBar.value;
+    if (wasPlaying) {
+        audioPlayer.play(); 
+    }
+});
